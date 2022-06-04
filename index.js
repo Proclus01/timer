ae8e144acf36abc00ce1fd0ef1b59154ba8f4372
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-04 15:24:19 -0700

    implement first pass at animation

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
+// Select the <circle /> SVG Element inside of our index.html file
+const circle = document.querySelector('circle');
+
+// Select the <circle /> element's perimeter
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+
+// Add the 'stroke-dasharray' SVG attribute to <circle />
+circle.setAttribute('stroke-dasharray', perimeter);
+
+// Initialize accumulator variable in order to modify 'stroke-dashoffset' attribute
+// using our callbacks that we add to the Timer class
+let currentOffset = 0;
+
 // Initialize our class
 // When we initialize our class we want to pass in a fourth extra parameter
 // which is an object containing three methods we want to call when an event occurs
@@ -15,8 +28,11 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
         console.log('Timer started');
     },
     onTick() {
-        //
-        console.log('Timer just ticked down');
+        // Set our SVG attribute
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+
+        // And then subtract from the current offset by an arbitrary amount
+        currentOffset = currentOffset - 50;
     },
     onComplete() {
         //
